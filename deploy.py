7d5ff5fb08aab7948f9c3db3bbb77d19b80d1ae9
Loïc Lopez <loic.lopez@epitech.eu>
2018-12-29 22:16:01 -0500

    redis deploy

--- deploy.py
+++ deploy.py
@@ -5,7 +5,7 @@ from lib import OptionParser, Container, Directory
 parser = OptionParser()
 parser.add_option("--website", action="store_true", dest="website")
 parser.add_option("--api", action='store_true', dest="api")
-parser.add_option("--redis", action='store_true', dest="api")
+parser.add_option("--redis", action='store_true', dest="redis")
 (opts, args) = parser.parse_args()
 
 kubectl_base_cmd = "kubectl apply -f "
@@ -19,8 +19,7 @@ elif opts.api:
     os.system(kubectl_base_cmd + directory + Container.PHP_FPM_API.toYaml())
     os.system(kubectl_base_cmd + directory + Container.NGINX_API.toYaml())
 elif opts.redis:
-    directory = Directory.API.toPath()
-    os.system(kubectl_base_cmd + directory + Container.REDIS_LIVE.toYaml())
-    os.system(kubectl_base_cmd + directory + Container.REDIS_STORE.toYaml())
+    os.system(kubectl_base_cmd + Container.REDIS_LIVE.toYaml())
+    os.system(kubectl_base_cmd + Container.REDIS_STORE.toYaml())
 else:
     parser.print_help()

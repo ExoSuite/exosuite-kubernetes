838c40e8259f8c2649d5ccc17547703f2c82a0b5
Loïc Lopez <loic.lopez@epitech.eu>
2019-01-04 21:47:27 -0500

    save

--- build.py
+++ build.py
@@ -1,38 +1,6 @@
 #!/usr/bin/env python3
 
-import os
-
-from lib import OptionParser, Enum, Container, Directory, ContainerType, Env
-
-
-class Token(Enum):
-    VERSION = '<VERSION>'
-    CONTAINER = '<CONTAINER>'
-    REGISTRY = '<REGISTRY>'
-    REGISTRY_SECRET = '<REGISTRY_SECRET>'
-    DIRECTORY = '<DIRECTORY>'
-
-
-class Template(Enum):
-    PHP_FPM = "exosuite-php-fpm.template.yaml"
-    NGINX = "exosuite-nginx.template.yaml"
-
-    def toPath(self):
-        return Directory.TEMPLATES.toPath() + self.value
-
-
-class RegistrySecret(Enum):
-    STAGING = "staging-registry"
-    PRODUCTION = "production-registry"
-
-
-class Registry(Enum):
-    STAGING = "dev.exosuite.fr:5000/exosuite"
-    PRODUCTION = "exosuite.fr:5000/exosuite"
-
-    def toRegistrySecret(self):
-        return RegistrySecret.STAGING if self == Registry.STAGING else RegistrySecret.PRODUCTION
-
+from lib import *
 
 parser = OptionParser()
 parser.add_option("--version", action="store", dest="version", default=None)
@@ -40,17 +8,27 @@ parser.add_option("--staging", action="store_true", dest="staging")
 parser.add_option("--production", action="store_true", dest="production")
 parser.add_option("--website", action="store_true", dest="website")
 parser.add_option("--api", action='store_true', dest="api")
+parser.add_option("--database", action='store_true', dest="database")
+parser.add_option("--redis", action='store_true', dest="redis")
 parser.add_option("--clean", action='store_true', dest="clean")
 (opts, args) = parser.parse_args()
 
 if opts.clean is None:
-    parser.check_required("--version")
+    if opts.redis is None and opts.database is None:
+        parser.check_required("--version")
     parser.check_required("--staging" if opts.staging is not None else "--production")
     parser.check_required("--production" if opts.production is not None else "--staging")
 
 
+def writeToFile(container: Container, currentEnv: Env, dockerFileContent: str, outputDir: Directory):
+    f = open(container.toYaml(outputDir, currentEnv), "w")
+    f.write(dockerFileContent)
+    f.close()
+    print("Generated " + container.toRelativeGeneratedFile(outputDir, currentEnv))
+
+
 def generateKubernetesDeployment(container: Container, selectedRegistry: Registry, currentEnv: Env):
-    template = Template.PHP_FPM if container.isPhpFpm() > 0 else Template.NGINX
+    template = Template.PHP_FPM if container.isPhpFpm() else Template.NGINX
     dockerFileContent = open(template.toPath()).read()
 
     registrySecret = selectedRegistry.toRegistrySecret()
@@ -59,15 +37,42 @@ def generateKubernetesDeployment(container: Container, selectedRegistry: Registr
         .replace(Token.VERSION.value, opts.version) \
         .replace(Token.CONTAINER.value, container.value) \
         .replace(Token.REGISTRY.value, selectedRegistry.value) \
-        .replace(Token.REGISTRY_SECRET.value, str(registrySecret))
+        .replace(Token.REGISTRY_SECRET.value, str(registrySecret)) \
+        .replace(Token.ENV.value, str(currentEnv))
 
     if container.isPhpFpm():
         dockerFileContent = dockerFileContent.replace(Token.DIRECTORY.value, container.toProjectDirectory())
 
     outputDir = Directory.API if opts.api else Directory.WEBSITE
-    f = open(container.toYaml(outputDir, currentEnv), "w")
-    f.write(dockerFileContent)
-    f.close()
+    writeToFile(container, currentEnv, dockerFileContent, outputDir)
+
+
+def generateRedisKubernetesDeployment(container: Container, currentEnv: Env):
+    template = Template.REDIS
+    dockerFileContent = open(template.toPath()).read()
+
+    dockerFileContent = dockerFileContent \
+        .replace(Token.CONTAINER.value, container.value) \
+        .replace(Token.ENV.value, currentEnv.value)
+
+    writeToFile(container, currentEnv, dockerFileContent, Directory.REDIS)
+
+
+def generateDatabaseKubernetesDeployment(container: Container, currentEnv: Env):
+    template = Template.POSTGRES
+    dockerFileContent = open(template.toPath()).read()
+
+    databaseSettings = container.toDatabaseSettings(currentEnv)
+
+    dockerFileContent = dockerFileContent \
+        .replace(Token.CONTAINER.value, container.value) \
+        .replace(Token.ENV.value, currentEnv.value)\
+        .replace(Token.DATABASE.value, databaseSettings[Token.DATABASE.key()])\
+        .replace(Token.DATABASE_USER.value, databaseSettings[Token.DATABASE_USER.key()]) \
+        .replace(Token.DATABASE_PASSWORD.value, databaseSettings[Token.DATABASE_PASSWORD.key()])\
+        .replace(Token.IMAGE.value, container.toDatabaseDockerImage())
+
+    writeToFile(container, currentEnv, dockerFileContent, Directory.DATABASE)
 
 
 env = Env.STAGING if opts.staging else Env.PRODUCTION
@@ -83,6 +88,12 @@ if opts.api:
 elif opts.website:
     generateKubernetesDeployment(Container.NGINX_WEBSITE, registry, env)
     generateKubernetesDeployment(Container.PHP_FPM_WEBSITE, registry, env)
+elif opts.redis:
+    generateRedisKubernetesDeployment(Container.REDIS_LIVE, env)
+    generateRedisKubernetesDeployment(Container.REDIS_STORE, env)
+elif opts.database:
+    generateDatabaseKubernetesDeployment(Container.POSTGRES_WEBSITE, env)
+    generateDatabaseKubernetesDeployment(Container.POSTGRES_API, env)
 elif opts.clean:
     os.system("rm -f ./build/api/* && rm -f ./build/website/*")
     print("Build directories cleaned!")
--- lib/__init__.py
+++ lib/__init__.py
@@ -13,7 +13,7 @@ class OptionParser(optparse.OptionParser):
 
 
 class Enum(EnumProvider):
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.value)
 
 
@@ -27,19 +27,26 @@ class Directory(Enum):
     WEBSITE = "build/website"
     TEMPLATES = "templates"
     REDIS = "redis"
+    DATABASE = "database"
 
     def toPath(self, env: Env = None):
         path = os.path.dirname(os.path.realpath(__file__))
-        path += "/../" + self.value + "/"
+        path += "/../" + self.value
+        path = os.path.normpath(path) + "/"
         if env is None:
             return path
-
         return path + str(env) + '/'
 
 
 class ContainerType(Enum):
     PHP_FPM = "php-fpm"
     NGINX = "nginx"
+    POSTGRES = "postgres"
+
+
+class DockerImage(Enum):
+    POSTGIS = "mdillon/postgis:11-alpine"
+    POSTGRES = "postgres:11.1-alpine"
 
 
 class Container(Enum):
@@ -49,22 +56,113 @@ class Container(Enum):
     NGINX_WEBSITE = "exosuite-website-nginx"
     REDIS_LIVE = "exosuite-redis-live"
     REDIS_STORE = "exosuite-redis-store"
+    POSTGRES_WEBSITE = "exosuite-website-postgres"
+    POSTGRES_API = "exosuite-users-api-postgres"
 
-    def toYaml(self, outputDir: Directory, env: Env = None):
-
+    def toYaml(self, outputDir: Directory, env: Env = None) -> str:
         return outputDir.toPath(env) + self.value + '.yaml'
 
-    def toProjectDirectory(self):
-        container_str = str(self)
-        pos = container_str.find(str(ContainerType.PHP_FPM))
-        if pos > 0:
-            return container_str[0:pos - 1] # -1 remove the '-' char
+    def toRelativeGeneratedFile(self, outputDir: Directory, env: Env = None) -> str:
+        directory = outputDir.toPath(env) + self.value + '.yaml'
+        path = os.path.dirname(os.path.realpath(__file__))
+        path += "/../"
+        path = os.path.normpath(path) + "/"
+        return directory.replace(path, '')
 
-        pos = container_str.find(str(ContainerType.NGINX))
-        return container_str[0:pos - 1] # -1 remove the '-' char
+    def toProjectDirectory(self) -> str:
+        container_str = str(self.value)
+        for containerType in ContainerType:
+            pos = container_str.find(str(containerType.value))
+            if pos > 0:
+                return container_str[0:pos - 1]  # -1 remove the '-' char
 
-    def isPhpFpm(self):
-        return str(self).find(str(ContainerType.PHP_FPM)) > 0
+    def isPhpFpm(self) -> bool:
+        return str(self.value).find(str(ContainerType.PHP_FPM)) > 0
 
-    def isNginx(self):
-        return str(self).find(str(ContainerType.NGINX)) > 0
+    def isNginx(self) -> bool:
+        return str(self.value).find(str(ContainerType.NGINX)) > 0
+
+    def toDatabaseSettings(self, env: Env):
+        container_str = str(self.value)
+        container_str = container_str.split("exosuite-")[1].split("-postgres")[0]
+        if container_str.find("api") > 0:
+            container_str = container_str.split("users-")[1]
+        return getattr(DatabaseSetting, env.value.upper())[container_str.upper()]
+
+    def toDatabaseDockerImage(self) -> str:
+        if str(self.value).find("api") > 0:
+            return str(DockerImage.POSTGIS.value)
+        return str(DockerImage.POSTGRES.value)
+
+
+class Token(Enum):
+    VERSION = '<VERSION>'
+    CONTAINER = '<CONTAINER>'
+    REGISTRY = '<REGISTRY>'
+    REGISTRY_SECRET = '<REGISTRY_SECRET>'
+    DIRECTORY = '<DIRECTORY>'
+    ENV = "<ENV>"
+    IMAGE = "<IMAGE>"
+    DATABASE = "<DATABASE>"
+    DATABASE_USER = "<DATABASE_USER>"
+    DATABASE_PASSWORD = "<DATABASE_PASSWORD>"
+
+    def key(self):
+        return str(self.value).replace("<", '').replace(">", '')
+
+
+class Template(Enum):
+    PHP_FPM = "exosuite-php-fpm.template.yaml"
+    NGINX = "exosuite-nginx.template.yaml"
+    REDIS = "exosuite-redis.template.yaml"
+    POSTGRES = "exosuite-postgres.template.yaml"
+
+    def toPath(self):
+        return Directory.TEMPLATES.toPath() + self.value
+
+
+class RegistrySecret(Enum):
+    STAGING = "staging-registry"
+    PRODUCTION = "production-registry"
+
+
+class Registry(Enum):
+    STAGING = "dev.exosuite.fr:5000/exosuite"
+    PRODUCTION = "exosuite.fr:5000/exosuite"
+
+    def toRegistrySecret(self):
+        return RegistrySecret.STAGING if self == Registry.STAGING else RegistrySecret.PRODUCTION
+
+
+class DatabaseSetting:
+    WEBSITE = "WEBSITE"
+    API = "API"
+
+    STAGING = {
+        API: {
+            Token.DATABASE.key(): Container.POSTGRES_API.toProjectDirectory(),
+            Token.DATABASE_PASSWORD.key(): "AyLVJWHp##LtSx4g%qYt",
+            Token.DATABASE_USER.key(): "exosuite"
+        },
+        WEBSITE: {
+            Token.DATABASE.key(): Container.POSTGRES_WEBSITE.toProjectDirectory(),
+            Token.DATABASE_PASSWORD.key(): "w8RP3#Ex++8EdHaH",
+            Token.DATABASE_USER.key(): "exosuite"
+        }
+    }
+
+    PRODUCTION = {
+        API: {
+            Token.DATABASE.key(): Container.POSTGRES_API.toProjectDirectory(),
+            'DATABASE_PASSWORD': "MAy5UBgd2YVgv8WTzSACQrGSddURGdzfpa9",
+            Token.DATABASE_USER.key(): "exosuite"
+        },
+        WEBSITE: {
+            Token.DATABASE.key(): Container.POSTGRES_WEBSITE.toProjectDirectory(),
+            'DATABASE_PASSWORD': "4tpaBrVqCqDXct7862VmFAmfrAvjpYuNReqJx7Snqy",
+            Token.DATABASE_USER.key(): "exosuite"
+        }
+    }
+
+    def __getitem__(self, item):
+        print(item, "hello")
